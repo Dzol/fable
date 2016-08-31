3dd0dd3426480fc4f48148b70babb3b246633181
Joseph Yiasemides <joseph.yiasemides@erlang-solutions.com>
2016-08-31 21:21:02 +0200

    Move the scanner code before the parser code

--- src/fable.erl
+++ src/fable.erl
@@ -16,6 +16,57 @@
 %% Interface
 %% -------------------------------------------------------------------
 
+%% @doc The `scan/1` procedure takes a string of characters and
+%% produces a list of tokens which are more amenable to processing,
+%% and thus parsing, by machine. The symbols `open` and `close`
+%% represent opening and closing parentheses respectively. They can be
+%% thought of as constants or enumerated constants in some sense. In
+%% fact that is exactly what they are underneath. They're here just
+%% for the intrinsic meaning we give them. Integer literals produce
+%% integer values right away and LISP atoms or symbols produce Erlang
+%% strings right away. We don't do string literals and we don't
+%% understand LISP quoting (if we really want it then we can build an
+%% interpreter or compiler with a procedure called `quote`). All these
+%% clauses push to the stack and press on with a recursive call to
+%% scan the remaining characters in the string.
+scan("") ->
+    %% When we see an empty string then we know there are no tokens in
+    %% it so we return the empty list.
+    [];
+scan([$(|Rest]) ->
+    %% An opening parenthesis, `$(`, produces the symbol `open`.
+    [open|scan(Rest)];
+scan([$)|Rest]) ->
+    %% An closing parenthesis, `$(`, produces the symbol `close`.
+    [close|scan(Rest)];
+scan([?SPACE|Rest]) ->
+    %% Space(s) is ignored: we should also ignore other white-space!
+    scan(Rest);
+scan([H|Rest])
+  when $! == H; $% == H; $* == H; $+ == H; $- == H;
+       $< == H; $= == H; $> == H; $^ == H; $~ == H ->
+    %% When we see one of a selection of operators then we call the
+    %% `operator` procedure to advance us one character which must be
+    %% a SPACE character (see the definition below). In other words we
+    %% don't allow LISP symbols or atoms to start with one of the
+    %% operators we recognise though they could start with one we
+    %% don't recognise.
+    [operator(Rest, H)|scan(Rest)];
+scan([H|Rest]) when $0 =< H, H =< $9 ->
+    %% When we see a digit we collect the like characters of an
+    %% integer by calling the `integer` procedure and then advance the
+    %% scanner. This really saves us work if we'd combined the scanner
+    %% and parser (but maybe that's what recursive-decent parses make
+    %% tidy).
+    {S, More} = integer(Rest, [H]),
+    [S|scan(More)];
+scan([H|Rest]) when $a =< H, H =< $z; $A =< H, H =< $Z ->
+    %% When we see a letter we collect the like characters of a LISP
+    %% symbol or atom by calling `symbol` and then advance the
+    %% scanner.
+    {S, More} = symbol(Rest, [H]),
+    [S|scan(More)].
+
 %% @doc The `parse/1` procedure takes an Erlang list of tokens like
 %% the list produced by `scan/1` and returns a parse tree. The parse
 %% tree is an Erlang list too, but unlike the one produced by `scan/1`
@@ -77,57 +128,6 @@ parse([X|M], S) ->
     %% it) into the tree `S`. These symbols don't give us any
     %% information about structure we can introduce.
     parse(M, S ++ [X]).
-
-%% @doc The `scan/1` procedure takes a string of characters and
-%% produces a list of tokens which are more amenable to processing,
-%% and thus parsing, by machine. The symbols `open` and `close`
-%% represent opening and closing parentheses respectively. They can be
-%% thought of as constants or enumerated constants in some sense. In
-%% fact that is exactly what they are underneath. They're here just
-%% for the intrinsic meaning we give them. Integer literals produce
-%% integer values right away and LISP atoms or symbols produce Erlang
-%% strings right away. We don't do string literals and we don't
-%% understand LISP quoting (if we really want it then we can build an
-%% interpreter or compiler with a procedure called `quote`). All these
-%% clauses push to the stack and press on with a recursive call to
-%% scan the remaining characters in the string.
-scan("") ->
-    %% When we see an empty string then we know there are no tokens in
-    %% it so we return the empty list.
-    [];
-scan([$(|Rest]) ->
-    %% An opening parenthesis, `$(`, produces the symbol `open`.
-    [open|scan(Rest)];
-scan([$)|Rest]) ->
-    %% An closing parenthesis, `$(`, produces the symbol `close`.
-    [close|scan(Rest)];
-scan([?SPACE|Rest]) ->
-    %% Space(s) is ignored: we should also ignore other white-space!
-    scan(Rest);
-scan([H|Rest])
-  when $! == H; $% == H; $* == H; $+ == H; $- == H;
-       $< == H; $= == H; $> == H; $^ == H; $~ == H ->
-    %% When we see one of a selection of operators then we call the
-    %% `operator` procedure to advance us one character which must be
-    %% a SPACE character (see the definition below). In other words we
-    %% don't allow LISP symbols or atoms to start with one of the
-    %% operators we recognise though they could start with one we
-    %% don't recognise.
-    [operator(Rest, H)|scan(Rest)];
-scan([H|Rest]) when $0 =< H, H =< $9 ->
-    %% When we see a digit we collect the like characters of an
-    %% integer by calling the `integer` procedure and then advance the
-    %% scanner. This really saves us work if we'd combined the scanner
-    %% and parser (but maybe that's what recursive-decent parses make
-    %% tidy).
-    {S, More} = integer(Rest, [H]),
-    [S|scan(More)];
-scan([H|Rest]) when $a =< H, H =< $z; $A =< H, H =< $Z ->
-    %% When we see a letter we collect the like characters of a LISP
-    %% symbol or atom by calling `symbol` and then advance the
-    %% scanner.
-    {S, More} = symbol(Rest, [H]),
-    [S|scan(More)].
 
 
 %% -------------------------------------------------------------------
